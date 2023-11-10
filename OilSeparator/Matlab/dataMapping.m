0e4a72865b66889b2b499e1280183dba4b291f01
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2023-11-10 21:22:56 +0100

    Data mapping, almost done. Final for today, got kicked out again :(

--- OilSeparator/Matlab/dataMapping.m
+++ OilSeparator/Matlab/dataMapping.m
@@ -29,3 +29,19 @@ legend('Level Transmitter 1', 'Location', 'southeast')
 
 %% Test 2 - Tank 2, water section
 
+figure
+title("Oil Separator: Data Logging - Tank 2 mapping")
+xlabel("Time [seconds]")
+hold on
+
+yyaxis left
+ylabel("Output [bits]")
+plot(Test2Tank2.Time0, ...
+    Test2Tank2.LT2percent, '-b');
+
+yyaxis right
+ylabel("Measured height [cm]")
+yline(0, '--r')
+axis([0 Test2Tank2.Time0(end) 0 17 ])
+
+legend('Level Transmitter 2', 'Location', 'southeast')
