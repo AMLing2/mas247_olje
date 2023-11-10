e272e757d354e01a58d778ee9b99269833edcb88
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2023-11-10 14:17:53 +0100

    Updated HMI, also added logged data. Have only implemented test 1 into Matlab.

---
+++ OilSeparator/Matlab/dataMapping.m
@@ -0,0 +1,31 @@
+clc; close all;
+
+% Project - Oil Separator: Plotting
+
+%% Test 1 - Tank 1
+
+figure
+title("Oil Separator: Data Logging - Tank 1 mapping")
+xlabel("Time [seconds]")
+hold on
+
+yyaxis left
+ylabel("Output [bits]")
+plot(Test1Tank1.Time0, ...
+    Test1Tank1.LT1percent, '-b');
+
+yyaxis right
+ylabel("Measured height [cm]")
+yline(0, '--r');
+yline(4.4, '--r');
+yline(5.7, '--r');
+yline(9.1, '--r');
+yline(11.1, '--r');
+yline(15.1, '--r');
+yline(16.9, '--r');
+axis([0 Test1Tank1.Time0(end) 3.7 17 ])
+
+legend('Level Transmitter 1', 'Location', 'southeast')
+
+%% Test 2 - Tank 2, water section
+
